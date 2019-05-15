9d7bdfbdf27368c0eb587aa27558eda74633b2f0
Imalka Gunawardana <imalkagunawardana1@gmail.com>
2019-05-16 03:09:27 +0530

    up

--- Backend/src/main/java/lk/vrs/Application.java
+++ Backend/src/main/java/lk/vrs/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@ComponentScan("lk.vrs")
-//@EnableJpaRepositories(basePackages = "lk.vrs.repository")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
