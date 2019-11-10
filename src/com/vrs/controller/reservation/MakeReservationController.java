58308514c7876e66888d2cbe7230aaef7e8bab72
Imalka Gunawardana <imalkagunawardana1@gmail.com>
2019-11-10 12:17:09 +0530

    up

--- src/com/vrs/controller/reservation/MakeReservationController.java
+++ src/com/vrs/controller/reservation/MakeReservationController.java
@@ -10,6 +10,10 @@ import com.vrs.entity.Reservation;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.spec.SecretKeySpec;
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,6 +26,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Base64;
 import java.util.Date;
+import java.util.Properties;
 
 @WebServlet(urlPatterns = "/makeReservation")
 public class MakeReservationController extends HttpServlet {
@@ -63,5 +68,52 @@ public class MakeReservationController extends HttpServlet {
         }
 
         resp.sendRedirect("view/customer/success_page.jsp?reservation=" + registrationId);
+    }
+
+    private void emailReservation(){
+        try {
+            //------------------------------Set gmail server as smtp mailing server-------------------------------------
+            Properties props = new Properties();
+            props.setProperty("mail.host", "smtp.gmail.com");
+            props.setProperty("mail.smtp.port", "587");
+            props.setProperty("mail.smtp.auth", "true");
+            props.setProperty("mail.smtp.starttls.enable", "true");
+
+            //----------------------------------------Login to email (sender)-------------------------------------------
+            Authenticator auth = new SMTPAuthenticator("webphpjava@gmail.com", "webphpjava1");
+
+            Session session = Session.getInstance(props, auth);
+
+            //--------------------------------------Create email text (body)--------------------------------------------
+            MimeMessage msg = new MimeMessage(session);
+//            msg.setText(String.valueOf(number));//---Set random number to email
+            msg.setSubject("Confirmation Code");//---Set subject
+            msg.setFrom(new InternetAddress("webphpjava@gmail.com"));//---Set email
+//            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(emailViaUid.getEmailAddress()));//---Set receiver's email
+
+            //------------------------------------------Send email------------------------------------------------------
+            Transport.send(msg);
+        } catch (AuthenticationFailedException ex) {//--Catch if any authentication exception occurred
+            ex.printStackTrace();
+        } catch (AddressException ex) {//--Catch if any address exception occurred
+            ex.printStackTrace();
+        } catch (MessagingException ex) {//--Catch if any messaging exception occurred
+            ex.printStackTrace();
+        }
+    }
+
+    //---------------------------------Authenticate (login to) email (sender)-------------------------------------------
+    private class SMTPAuthenticator extends Authenticator {
+
+        private PasswordAuthentication authentication;
+
+        public SMTPAuthenticator(String login, String password) {
+            authentication = new PasswordAuthentication(login, password);
+        }
+
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return authentication;
+        }
     }
 }
