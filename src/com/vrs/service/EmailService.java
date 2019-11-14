5811957f5bdb60df0757e6dc2bac07afe5f67df5
Imalka Gunawardana <imalkagunawardana1@gmail.com>
2019-11-14 23:11:44 +0530

    up

--- src/com/vrs/service/EmailService.java
+++ src/com/vrs/service/EmailService.java
@@ -3,9 +3,7 @@ package com.vrs.service;
 import com.vrs.entity.Reservation;
 
 import javax.mail.*;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.mail.internet.*;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Properties;
@@ -15,22 +13,36 @@ public class EmailService {
         try {
             //------------------------------Set gmail server as smtp mailing server-------------------------------------
             Properties props = new Properties();
+
+//            props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+//            props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
+//            props.put("mail.smtp.socketFactory.class",
+//                    "javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
+//            props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
+//            props.put("mail.smtp.port", "465"); //SMTP Port
+
             props.setProperty("mail.host", "smtp.gmail.com");
             props.setProperty("mail.smtp.port", "587");
             props.setProperty("mail.smtp.auth", "true");
             props.setProperty("mail.smtp.starttls.enable", "true");
 
             //----------------------------------------Login to email (sender)-------------------------------------------
-            Authenticator auth = new SMTPAuthenticator("webphpjava@gmail.com", "webphpjava123");
+//            Authenticator auth = new SMTPAuthenticator("webphpjava@gmail.com", "webphpjava123");
 
-            Session session = Session.getInstance(props, auth);
-
+//            Session session = Session.getInstance(props, auth);
+            Session session = Session.getInstance(props,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication("rajapaksap1@gmail.com", "Priyantha123");
+                        }
+                    });
             //--------------------------------------Create email text (body)--------------------------------------------
             MimeMessage msg = new MimeMessage(session);
+            msg.addHeader("Content-type", "text/html; charset=UTF-8");
 //            msg.setText(String.valueOf(number));//---Set random number to email
 
             String htmlMessage = "" +
-                    "<div style='font-size:15px'>" +
+                    "<div style='font-size:14px'>" +
                     "<span style='font-weight:bold'>Reservation Number</span><span> - R" + savedRegistration.getId() + "</span><br>" +
                     "<span style='font-weight:bold'>Customer</span><span> - " + savedRegistration.getReservationCustomer().getCustomerName() + "</span><br>" +
                     "<span style='font-weight:bold'>Pickup From</span><span> - " + savedRegistration.getReservationPlaceFrom().getPlaceName() + "</span><br>" +
@@ -44,10 +56,18 @@ public class EmailService {
                     "<span style='font-weight:bold'>No of passengers</span><span> - " + (savedRegistration.getReservationAdults() + savedRegistration.getReservationChildren() + savedRegistration.getReservationInfants()) + "</span><br>" +
                     "<span style='font-weight:bold;font-size:24px'>Total Cost</span><span style='font-weight:bold;font-size:24px'> - &euro;" + String.format("%.2f", savedRegistration.getReservationPassenger().getPassengersPrice()) + "</span>" +
                     "</div>";
+            BodyPart messageBodyPart = new MimeBodyPart();
 
-            msg.setContent(htmlMessage, "text/html");
-            msg.setSubject("Trip Reservation Bill");//---Set subject
-            msg.setFrom(new InternetAddress("webphpjava@gmail.com", "Trip Reservation Bill"));//---Set email
+            // Fill the message
+            messageBodyPart.setContent(htmlMessage, "text/html; charset=UTF-8");
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+//            msg.setText("abc");
+            msg.setContent(multipart);
+            msg.setSubject("Trip Reservation");//---Set subject
+            msg.setFrom(new InternetAddress("rajapaksap1@gmail.com", "Trip Reservation Bill"));//---Set email
             msg.addRecipient(Message.RecipientType.TO, new InternetAddress(emailAddress));//---Set receiver's email
 
             //------------------------------------------Send email------------------------------------------------------
@@ -64,17 +84,17 @@ public class EmailService {
     }
 
     //---------------------------------Authenticate (login to) email (sender)-------------------------------------------
-    private class SMTPAuthenticator extends Authenticator {
-
-        private PasswordAuthentication authentication;
-
-        public SMTPAuthenticator(String login, String password) {
-            authentication = new PasswordAuthentication(login, password);
-        }
-
-        @Override
-        protected PasswordAuthentication getPasswordAuthentication() {
-            return authentication;
-        }
-    }
+//    private class SMTPAuthenticator extends Authenticator {
+//
+//        private PasswordAuthentication authentication;
+//
+//        public SMTPAuthenticator(String login, String password) {
+//            authentication = new PasswordAuthentication(login, password);
+//        }
+//
+//        @Override
+//        protected PasswordAuthentication getPasswordAuthentication() {
+//            return authentication;
+//        }
+//    }
 }
